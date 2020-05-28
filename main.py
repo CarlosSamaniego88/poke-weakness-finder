5ad16694355b1a776281be9f3bf2a16833c53655
Carlos Samaniego <cscarlossamaniego@gmail.com>
2020-05-28 18:57:13 -0400

    updated req and was able to return weaknesses in html

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import requests
+from bs4 import BeautifulSoup
 
 def main():
-    response = requests.get('https://pokemondb.net/pokedex/sceptile')
+    pokemon_to_exploit = input('Enter a Pokemon for its weaknesses:\n')
+    url = 'https://pokemondb.net/pokedex/' + pokemon_to_exploit
+    response = requests.get(url)
+    weaknesses_html = []
     if response.status_code == 200:
         print('Success!')
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        tables = soup.find_all(class_='type-table type-table-pokedex')
+        # print(tables)
+        for table in tables:
+            rows = table.find_all('tr')
+            for row in rows:
+                target_column = row.find_all('td', 'type-fx-cell type-fx-200')
+                weaknesses_html.append(target_column)    
+    
     elif response.status_code == 404:
         print('Not Found.')
-    print(response.content)
+    
+    for item in weaknesses_html:
+        if item == []:
+            weaknesses_html.remove(item)
+        else:
+            pass
+    print(weaknesses_html)
 if __name__ == '__main__':
     main() 
