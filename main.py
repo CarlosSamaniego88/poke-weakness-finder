aec35317746ce9cd1107e6fd86a4735f8345865e
Carlos Samaniego <cscarlossamaniego@gmail.com>
2020-05-28 20:53:51 -0400

    cleaned out html

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ def main():
     pokemon_to_exploit = input('Enter a Pokemon for its weaknesses:\n')
     url = 'https://pokemondb.net/pokedex/' + pokemon_to_exploit
     response = requests.get(url)
+    print('')
+    print('WEAKNESSES:')
     weaknesses_html = []
     if response.status_code == 200:
-        print('Success!')
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
         tables = soup.find_all(class_='type-table type-table-pokedex')
@@ -25,7 +26,20 @@ def main():
         if item == []:
             weaknesses_html.remove(item)
         else:
+            #item = item.replace('<td class="type-fx-cell type-fx-200" title=','')
             pass
-    print(weaknesses_html)
+    poke_weaknesses = []
+    for weakness in weaknesses_html:
+        for inner in weakness:
+            inner = str(inner)
+            left_item = inner.replace('<td class="type-fx-cell type-fx-200" title="','')
+            right_item = left_item.replace(' = super-effective">2</td>','')
+            #print(right_item)
+            item_array = right_item.split()
+            #print(item_array)
+            poke_weaknesses.append(item_array[0])
+
+    for weakness in poke_weaknesses:
+        print(weakness)
 if __name__ == '__main__':
     main() 
